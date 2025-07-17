9e3c37e98adf716dd347e50f1339c991c020a73f
balaji-githubstore <balajidinakaran1@gmail.com>
2025-07-17 15:23:52 +0530

    first commit

---
+++ CSharpConcept/Calculator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpConcept
+{
+    public class Calculator
+    {
+        public void Add(int a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+        public void Add(int a, int b, int c)
+        {
+            Console.WriteLine(a + b+c);
+        }
+        public void Add(int a, double b)
+        {
+            Console.WriteLine(a + b );
+        }
+
+        public void Add( double b, int a)
+        {
+            Console.WriteLine(a + b);
+        }
+    }
+
+
+    public class Runner
+    {
+        static void Main2(string[] args)
+        {
+            Calculator calculator = new Calculator();
+            calculator.Add(10, 10);
+        }
+    }
+}
